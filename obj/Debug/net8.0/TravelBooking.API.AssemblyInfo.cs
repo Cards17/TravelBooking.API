7c7ff109f2ad976c2333649317e4e5cfbbfdb65c
Liene <ricardo.piquero.jr@hotmail.com>
2024-07-15 21:06:48 +0800

    API

--- obj/Debug/net8.0/TravelBooking.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/TravelBooking.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelBooking.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6817d5c8cb2218d594c6ed29bf5653ae3f175c48")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelBooking.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelBooking.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
