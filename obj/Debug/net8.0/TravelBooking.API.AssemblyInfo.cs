6a6e5e9b9f1d55dfa554660cb77eba94b46bd71e
Ricardo Jr Piquero <ricardo.piquero.jr@hotmail.com>
2024-07-15 23:17:32 +0800

    added class

--- obj/Debug/net8.0/TravelBooking.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/TravelBooking.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelBooking.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c7ff109f2ad976c2333649317e4e5cfbbfdb65c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a544a58c64f980d8af465b4a67574fc10ddb9e70")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelBooking.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelBooking.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
